6ca8b67c8e7f07c8496df2048d417c748cb28326
Tomek Cichon <tomek1149@gmail.com>
2026-02-18 21:46:55 +0100

    test: prettier should format

--- src/views/TicketDetailView.vue
+++ src/views/TicketDetailView.vue
@@ -1,11 +1,9 @@
-<script setup>
-
+<script setup lang="ts">
+const x = 1
+const y = 'hello'
+const z = [1, 2, 3]
 </script>
 
 <template>
-test
+  <div>{{ x }}{{ y }}{{ z }}</div>
 </template>
-
-<style scoped>
-
-</style>
